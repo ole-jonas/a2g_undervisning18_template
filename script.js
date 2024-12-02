0eaf5b71722a9cdc76cc6cfe1accba3e9efbe5fe
ole-jonas <olejonaskoder@gmail.com>
2024-12-02 14:06:13 +0100

    Remove Button

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 // Reference to HTML elements
-const  = document.querySelector("#");
-const  = document.querySelector("#");
-const  = document.querySelector("#");
+const inputField = document.querySelector("#inputField");
+const list = document.querySelector("#list");
+const addButton = document.querySelector("#addButton");
 
 // Function to add a new item to the list
 function addItem() {
-  const  = .value;
+  const inputFieldvalue = inputField.trim();
+if (inputFieldValue === "") {
+  alert("Please enter a Valid item!");
+  return;
+}
 
     // Create a new list item and give content
-    const  = document.createElement("");
-    .textContent = ;
+    const li = document.createElement("li");
+    li.textContent = inputFieldValue;
 
     // Create a remove button for the new list item
-    const  = document.createElement("");
-    .textContent = "Slett";
-    .classList.add("");
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Slett";
+    removeButton.classList.add("removeButton");
 
     // Add an event listener to the remove button
-    .addEventListener("click", function () {
+    removeButton.addEventListener("click", function () {
       removeItem(li)
     });
 
     // Append the remove button to the list item
-    li.appendChild();
+    li.appendChild(removeButton);
 
     // Append the list item to the list
-    list.appendChild();
+    list.appendChild(li);
+
+    inputField.value = "";
 }
 
 // Function to remove an item from the list
